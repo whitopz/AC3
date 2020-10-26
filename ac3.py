46a6199d26c5f94bbc59e2a0e761de713d62efec
whitopz <dani.targino2@gmail.com>
2020-10-26 20:34:56 -0300

    AC3

--- ac3.py
+++ ac3.py
@@ -3,31 +3,28 @@ from flask import flask, jsonify, request
 from math import sqrt
 
 app = flask(__name__)
-
 @app.route('/')
 
+def primo(num):
+    if num != 1:
+        t = True
+        for i in range(2,num):
+             if num % i == 0:
+                t = False
+                break
+        return t
 not_p = []
 e = []
 lim = 100
-
-def primo(num):
-   if num != 1:
-         t = True
-         for i in range(2,num):
-             if num % i == 0:
-                 t = False
-                 break
-         return t
- 
 for num in range(2, lim + 1):
-   test = primo(num)
-   if (test):
-       e.append(num)
-   else:
-       not_p.append(num)
+    test = primo(num)
+    if (test):
+        e.append(num)
+    else:
+        not_p.append(num)
 
-print "Números primos: ", e
-print "Não são primos: ", not_p
+print ("Números primos: ", e)
+print ("Não são primos: ", a)
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
